bdda240db79c99808ec5c6bce7447d939db2ee97
sayan24 <samir.plays.742@gmail.com>
2023-07-29 11:02:46 +0530

    added slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LandingComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
