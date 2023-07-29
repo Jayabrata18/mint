2033f13f8745c7c4bca73f6058dd5cda711e69a0
sayan24 <samir.plays.742@gmail.com>
2023-07-29 22:01:08 +0530

    small change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,7 @@ import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: "home",
-    pathMatch: 'full'
-  },
-  {
-    path: "home",
+    path: "",
     component: HomeComponent
   },
   {
