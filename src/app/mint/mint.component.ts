d35a1e2d5901d4a9657eb780f5cf4fb4484b84d5
Jayabrata Pramanik <jayabrata180402@gmail.com>
2023-08-19 16:22:19 +0530

    add mint

--- src/app/mint/mint.component.ts
+++ src/app/mint/mint.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-mint',
   templateUrl: './mint.component.html',
-  styleUrls: ['./mint.component.scss']
+  styleUrls: ['./mint.component.scss'],
 })
 export class MintComponent {
   
