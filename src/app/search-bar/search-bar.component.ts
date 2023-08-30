047046bfa1e155c00d4f3d0f27255e1789062491
sayan24 <samir.plays.742@gmail.com>
2023-08-30 19:42:52 +0530

    added sheet link

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class SearchBarComponent {
   constructor(private httpClient: HttpClient) { }
   emailId : string = "";
-  url : string = "ADD_SCRIPT_URL_HERE";
+  url : string = "https://script.google.com/macros/s/AKfycbxOFAWt3O2TO_NKf2jgzgpHU_2UC1_6T7bbh370AzLyXn38eN7N5wdCTNzhawgVP4gZ3w/exec";
   emailEntry(){
     console.log(this.emailId);
     let params = new HttpParams().set("email",this.emailId);
